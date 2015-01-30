78929e5032c013933796e8a54567df3cdf92daab
Jared Williams <jaredwilliamsmail@gmail.com>
2015-01-30 13:25:40 -0600

    panels are smaller commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 //= require jquery
 //= require jquery_ujs
+//= require masonry/jquery.masonry
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require turbolinks
-//= require jquery.remotipart
+//= require masonry/jquery.masonry
 //= require_tree .
