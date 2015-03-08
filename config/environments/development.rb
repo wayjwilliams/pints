85ce3b4d3e3dbc0baeb45a18c3eb79b9e3f5740c
Jared Williams <jaredwilliamsmail@gmail.com>
2015-03-07 18:51:23 -0600

    pc validates error commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
