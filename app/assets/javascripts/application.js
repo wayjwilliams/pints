6d65e9df9e5a4d7a8a491631d5e97e7ef3746d21
Jared Williams <jaredwilliamsmail@gmail.com>
2015-03-07 22:52:34 -0600

    masonry coffee to js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,15 @@
 //= require jquery.turbolinks
 //= require turbolinks
 //= require masonry/jquery.masonry
+//= require masonry/modernizr-transitions
 //= require_tree .
+
+$(function(){
+  $('#pints').masonry({
+    itemSelector: '.box',
+    columnWidth: 200,
+    isAnimated: !Modernizr.csstransitions,
+    isFitWidth: true
+  });
+
+});
