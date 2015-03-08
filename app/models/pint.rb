5475fae3b5a034ebdae3248e12cd5e8afec3b54d
Jared Williams <jaredwilliamsmail@gmail.com>
2015-03-07 21:36:28 -0600

    attr update

--- app/models/pint.rb
+++ app/models/pint.rb
@@ -1,6 +1,6 @@
 class Pint < ActiveRecord::Base
   belongs_to :user
+  attr_accessible :image
   has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100" }
-
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
