40b375252a91c6b1310aa4c11b1b2ed88cbc86f6
Jared Williams <jaredwilliamsmail@gmail.com>
2015-03-07 21:44:20 -0600

    image commit

--- app/models/pint.rb
+++ app/models/pint.rb
@@ -1,6 +1,8 @@
 class Pint < ActiveRecord::Base
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   belongs_to :user
-  attr_accessible :image
-  has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
 end
